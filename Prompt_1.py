01926cc3454810d1773b0174956d293ae3a16598
hmmercer17 <hudsonmercer11@gmail.com>
2026-04-01 15:00:09 -0700

    Add files via upload

---
+++ Prompt_1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 14:33:23 2026
+
+@author: hudso
+"""
+
+def PrintPrompt1():
+    print("My name is Hudson Mercer, my pronouns are he/they. My favorite movie is The Nightmare Before Christmas because I really enjoy the creepy designs. My favorite food is pho.")
+    
+def main():
+    PrintPrompt1()
+    
+if __name__ == "__main__":
+    main()
+    
+    
