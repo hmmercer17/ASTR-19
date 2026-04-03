43e50c867736b14677d45a36b8fe0fb1105ec35c
hmmercer17 <hudsonmercer11@gmail.com>
2026-04-02 17:05:10 -0700

    Add files via upload

---
+++ Prompt_2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 16:52:41 2026
+
+@author: hudso
+"""
+
+x = 14.2
+y = 9.9
+print("x+y=",x+y)
+print(type(x+y))
+
+
+i = 2
+j = 4
+print("j-i=",j-i)
+print(type(j-i))
+
+
+a = 17.1
+b = 3
+print("a*b=",a*b)
+print(type(a*b))
+
+
